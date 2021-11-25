f5112535678781f3805241343579b0f9c1ba6252
CongKien <74692561+kckien16@users.noreply.github.com>
2021-11-25 21:57:17 +0700

    UD Home

--- src/screens/homes/HomeScreen.js
+++ src/screens/homes/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import {
   FlatList,
   ScrollView,
@@ -7,205 +7,205 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome5';
-import CategoryItem from '../../components/CategoryItem';
-import DetailItem from '../../components/DetailItem';
-import MainItem from '../../components/MainItem';
-import RecentItem from '../../components/RecentItem';
+} from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome5";
+import CategoryItem from "../../components/CategoryItem";
+import DetailItem from "../../components/DetailItem";
+import MainItem from "../../components/MainItem";
+import RecentItem from "../../components/RecentItem";
 
-
-import Color from '../../constants/Colors';
+import Color from "../../constants/Colors";
 
 const Main = [
   {
     id: 1,
-    name: 'Italian Pizza',
-    place: '48 Soba Basha, Alexandria',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "48 Soba Basha, Alexandria",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
   },
   {
     id: 2,
-    name: 'Italian Pizza',
-    place: '48 Soba Basha, Alexandria',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "48 Soba Basha, Alexandria",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
   },
   {
     id: 3,
-    name: 'Italian Pizza',
-    place: '48 Soba Basha, Alexandria',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "48 Soba Basha, Alexandria",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
   },
 ];
 
 const Detail = [
   {
     id: 1,
-    name: 'Chicken',
-    uri: 'https://cdn-icons-png.flaticon.com/256/4359/4359633.png',
+    name: "Chicken",
+    uri: "https://cdn-icons-png.flaticon.com/256/4359/4359633.png",
   },
   {
     id: 2,
-    name: 'Chicken',
-    uri: 'https://cdn-icons-png.flaticon.com/256/4359/4359633.png',
+    name: "Chicken",
+    uri: "https://cdn-icons-png.flaticon.com/256/4359/4359633.png",
   },
   {
     id: 3,
-    name: 'Chicken',
-    uri: 'https://cdn-icons-png.flaticon.com/256/4359/4359633.png',
+    name: "Chicken",
+    uri: "https://cdn-icons-png.flaticon.com/256/4359/4359633.png",
   },
   {
     id: 4,
-    name: 'Chicken',
-    uri: 'https://cdn-icons-png.flaticon.com/256/4359/4359633.png',
+    name: "Chicken",
+    uri: "https://cdn-icons-png.flaticon.com/256/4359/4359633.png",
   },
   {
     id: 5,
-    name: 'Chicken',
-    uri: 'https://cdn-icons-png.flaticon.com/256/4359/4359633.png',
+    name: "Chicken",
+    uri: "https://cdn-icons-png.flaticon.com/256/4359/4359633.png",
   },
 ];
 
 const Recent = [
   {
     id: 1,
-    name: 'Italian Pizza',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    status: 'Fresh hamburger white chicken, salad, tomatoes',
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
+    status: "Fresh hamburger white chicken, salad, tomatoes",
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
   },
   {
     id: 2,
-    name: 'Italian Pizza',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    status: 'Fresh hamburger white chicken, salad, tomatoes',
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
+    status: "Fresh hamburger white chicken, salad, tomatoes",
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
   },
   {
     id: 3,
-    name: 'Italian Pizza',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    status: 'Fresh hamburger white chicken, salad, tomatoes',
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
+    status: "Fresh hamburger white chicken, salad, tomatoes",
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
   },
 ];
 
 const ListTab = [
   {
-    status: 'Near you',
+    status: "Near you",
   },
   {
-    status: 'Newest',
+    status: "Newest",
   },
   {
-    status: 'Best rated',
+    status: "Best rated",
   },
   {
-    status: 'Trending',
+    status: "Trending",
   },
 ];
 const ListData = [
   {
     id: 1,
-    name: 'Hamburger',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Hamburger",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
-    detail: 'Restaurants',
-    status: 'Near you',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
+    detail: "Restaurants",
+    status: "Near you",
   },
   {
     id: 6,
-    name: 'Hamburger',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Hamburger",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
-    detail: 'Restaurants',
-    status: 'Near you',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
+    detail: "Restaurants",
+    status: "Near you",
   },
   {
     id: 7,
-    name: 'Hamburger',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Hamburger",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
-    detail: 'Restaurants',
-    status: 'Near you',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
+    detail: "Restaurants",
+    status: "Near you",
   },
   {
     id: 2,
-    name: 'Topoki',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Topoki",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
-    detail: 'Restaurants',
-    status: 'Newest',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
+    detail: "Restaurants",
+    status: "Newest",
   },
   {
     id: 3,
-    name: 'Italian Pizza',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Italian Pizza",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
-    detail: 'Restaurants',
-    status: 'Best rated',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
+    detail: "Restaurants",
+    status: "Best rated",
   },
   {
     id: 4,
-    name: 'Milk Tea',
-    place: '200m',
-    assess: '4.8',
-    time: '15:25',
+    name: "Milk Tea",
+    place: "200m",
+    assess: "4.8",
+    time: "15:25",
     price: 3000,
-    uri: 'https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/',
-    detail: 'Restaurants',
-    status: 'Trending',
+    uri: "https://anhdep.tv/attachments/143b4a92fa369f3beee2c93bfe2990c7-jpeg.6915/",
+    detail: "Restaurants",
+    status: "Trending",
   },
 ];
 const HomeScreen = () => {
   const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch('http://10.0.2.2:8080/product')
-        .then(response => response.json())
-        .then(json => setData(json))
-        .catch(err => console.log(err));
-}, []);
-console.log('data:  ', data);
+  // useEffect(() => {
+    
+  // }, []);
+  fetch("http://10.0.2.3:8080/api/products")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((err) => console.log(err));
+  console.log("data:  ", data);
 
-  const [status, setStatus] = useState('All');
+  const [status, setStatus] = useState("All");
   const [dataList, setDataList] = useState(ListData);
 
-  const setStatusFilter = status => {
-    if (status !== 'All') {
-      setDataList([...ListData.filter(e => e.status === status)]);
+  const setStatusFilter = (status) => {
+    if (status !== "All") {
+      setDataList([...ListData.filter((e) => e.status === status)]);
     } else {
       setDataList(ListData);
     }
@@ -233,7 +233,8 @@ console.log('data:  ', data);
             <FontAwesome name="search" size={20} color="#F55A00" />
             <TextInput
               style={styles.inputSearch}
-              placeholder="Search for food"></TextInput>
+              placeholder="Search for food"
+            ></TextInput>
           </View>
           <View style={styles.viewIconOption}>
             <FontAwesome name="sliders-h" size={20} color="#F55A00" />
@@ -243,16 +244,19 @@ console.log('data:  ', data);
           <FlatList
             horizontal
             data={data}
-            renderItem={({item}) => (
+            renderItem={({ item }) => (
               <MainItem
                 // image={item.uri}
-                name={item.name}
+                name={item.product_name}
                 // place={item.place}
                 // assess={item.assess}
                 // time={item.time}
                 // price={item.price}
                 // status={item.status}
               />
+              // <View style={{flex:1}}>
+              //   <Text style={{fontSize:16}}>{item.name}</Text>
+              // </View>
             )}
           />
         </View>
@@ -260,7 +264,7 @@ console.log('data:  ', data);
           <FlatList
             horizontal
             data={Detail}
-            renderItem={({item}) => (
+            renderItem={({ item }) => (
               <DetailItem image={item.uri} name={item.name} />
             )}
           />
@@ -275,7 +279,7 @@ console.log('data:  ', data);
           <FlatList
             horizontal
             data={Recent}
-            renderItem={({item}) => (
+            renderItem={({ item }) => (
               <RecentItem
                 image={item.uri}
                 name={item.name}
@@ -289,18 +293,20 @@ console.log('data:  ', data);
           />
         </View>
         <View style={styles.listTab}>
-          {ListTab.map(e => (
+          {ListTab.map((e) => (
             <TouchableOpacity
               style={[
                 styles.btnTab,
                 status === e.status && styles.btnTabActive,
               ]}
-              onPress={() => setStatusFilter(e.status)}>
+              onPress={() => setStatusFilter(e.status)}
+            >
               <Text
                 style={[
                   styles.textTab,
                   status === e.status && styles.textTabActive,
-                ]}>
+                ]}
+              >
                 {e.status}
               </Text>
             </TouchableOpacity>
@@ -308,7 +314,7 @@ console.log('data:  ', data);
         </View>
         <FlatList
           data={dataList}
-          renderItem={({item}) => (
+          renderItem={({ item }) => (
             <CategoryItem
               image={item.uri}
               name={item.name}
@@ -332,26 +338,26 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: 40,
     paddingRight: 20,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
   },
   viewPlace: {
     marginTop: 20,
-    width: '100%',
-    flexDirection: 'row',
+    width: "100%",
+    flexDirection: "row",
   },
   viewIconPlace: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   viewIconNotifi: {
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   textPlace: {
     fontSize: 16,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontFamily: 'Hellix',
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontFamily: "Hellix",
     marginLeft: 15,
   },
   viewTitleMain: {
@@ -360,32 +366,32 @@ const styles = StyleSheet.create({
   },
   titleMain: {
     fontSize: 28,
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-    fontFamily: 'Hellix',
+    fontWeight: "bold",
+    fontStyle: "normal",
+    fontFamily: "Hellix",
   },
   viewSearch: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   cardSearch: {
-    width: '90%',
+    width: "90%",
     height: 40,
-    alignItems: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#EEEEEE',
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#EEEEEE",
     borderRadius: 8,
     paddingLeft: 15,
   },
   viewIconOption: {
-    width: '10%',
+    width: "10%",
     margin: 10,
   },
   inputSearch: {
     fontSize: 14,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontFamily: 'Hellix',
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontFamily: "Hellix",
     marginLeft: 10,
   },
   listMain: {
@@ -394,34 +400,34 @@ const styles = StyleSheet.create({
   listDetail: {
     marginTop: 20,
     borderBottomWidth: 0.2,
-    borderColor: '#000',
+    borderColor: "#000",
     paddingBottom: 20,
   },
   textRecent: {
     flex: 1,
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "flex-end",
     marginTop: 20,
     marginBottom: 20,
   },
   titleRecent: {
     fontSize: 21,
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontFamily: 'Hellix',
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontFamily: "Hellix",
   },
   titleSeeAll: {
     fontSize: 16,
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontFamily: 'Hellix',
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontFamily: "Hellix",
     color: Color.orange,
     right: 10,
   },
   btnSeeAll: {
     flex: 1,
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
   },
   listRecent: {
     flex: 1,
@@ -429,19 +435,19 @@ const styles = StyleSheet.create({
   },
   listTab: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   btnTab: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 20,
     margin: 5,
   },
   textTab: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   textTabActive: {
     color: Color.orange,
