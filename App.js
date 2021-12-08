06dbacb6651cfac2adc23d81516fe661f4b05e52
huyhq01 <huywoaytai@gmail.com>
2021-12-08 23:34:09 +0700

    fix Screen

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './src/screens/SignIn'
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -5,13 +9,6 @@ import HomeScreen from "./src/screens/homes/HomeScreen";
 import CartScreen from "./src/screens/orders/CartScreen";
 import ItemScreen from "./src/screens/orders/ItemScreen";
 import MyTabs from "./src/navigations/NavigationTab";
-import SignIn from "./src/screens/SignIn";
-import Register from "./src/screens/Register";
-import OrderSceen from "./src/screens/orders/OrderSceen";
-import WorldTree from "./src/screens/WorldTree";
-import UpdateProfileScreen from "./src/screens/profiles/UpdateProfileScreen";
-import Menu from "./src/screens/homes/Menu";
-
 
 
 const Stack = createNativeStackNavigator();
@@ -19,9 +16,12 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="World">
-        <Stack.Screen name="Orders" component={OrderSceen} />
-        <Stack.Screen name="Home" component={MyTabs} options={{ headerShown: false }}/>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={MyTabs}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="CartScreen" component={CartScreen} />
         <Stack.Screen name="ItemScreen" component={ItemScreen} />
       </Stack.Navigator>
